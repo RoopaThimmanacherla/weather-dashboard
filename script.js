56cbfe4b19c9ac3a84a7c62bdd57d1aa16346db9
Roopa Thimmanacherla <roopareddy90@gmail.com>
2023-11-10 10:34:33 -0800

    added code to retrive current day weather

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ var searchFormEl = document.querySelector("#search-form");
 var WeatherAPIKey = "dd722ae2b0b3e5ffb448c9a00bf261d0";
 var latitude;
 var longitude;
+var currentWind;
+var currentHumidity;
+var currentTemp;
+var today;
+
 function handleSearchFormSubmit(event) {
   event.preventDefault();
   var searchCityEl = document.querySelector("#search-input").value;
@@ -26,10 +31,36 @@ function handleSearchFormSubmit(event) {
     })
     .then(function (geocode) {
       console.log(geocode);
-      console.log(geocode[0].lat);
-      console.log(geocode[0].lon);
       latitude = geocode[0].lat;
       longitude = geocode[0].lon;
+      console.log(latitude);
+      console.log(longitude);
+    });
+
+  var currentWeatherUrl =
+    "https://api.openweathermap.org/data/2.5/weather?lat=" +
+    latitude +
+    "&lon=" +
+    longitude +
+    "&appid=" +
+    WeatherAPIKey;
+
+  fetch(currentWeatherUrl)
+    .then(function () {
+      if (!response.ok) {
+        console.log(response.status);
+        throw response.json();
+      }
+      return response.json();
+    })
+    .then(function (currentWeather) {
+      console.log(currentWeather);
+      currentWind = currentWeather.visibility.wind;
+      console.log(currentWind);
+      currentHumidity = currentWeather.main.humidity.unit;
+      console.assertlog(currentHumidity);
+      currentTemp = currentWeather.main.temp;
+      today = new Date().toLocaleDateString();
     });
 }
 searchFormEl.addEventListener("submit", handleSearchFormSubmit);
